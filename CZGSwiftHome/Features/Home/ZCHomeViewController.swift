dd25bb857133c5f8a17097792a63e24b42bee32b
zhangchuangchuang <1374618878@qq.com>
2019-08-14 20:51:21 +0800

    创建 4个模块的 视图

---
+++ CZGSwiftHome/Features/Home/ZCHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ZCHomeViewController.swift
+//  CZGSwiftHome
+//
+//  Created by 张闯闯 on 2019/8/14.
+//  Copyright © 2019 张闯闯. All rights reserved.
+//
+
+import UIKit
+
+class ZCHomeViewController: ZCBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CZGSwiftHome/Features/detail/ZCDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ZCDetailViewController.swift
+//  CZGSwiftHome
+//
+//  Created by 张闯闯 on 2019/8/14.
+//  Copyright © 2019 张闯闯. All rights reserved.
+//
+
+import UIKit
+
+class ZCDetailViewController: ZCBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      self.view.backgroundColor = UIColor.yellow
+      self.title = "详情界面"
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
